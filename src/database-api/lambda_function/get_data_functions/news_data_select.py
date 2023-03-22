58dea1c6f5e7969746a0e66356c636adec54ce61
tc-huang <jameshuang890902@gmail.com>
2023-03-22 11:23:11 +0800

    chore: Update

--- src/database-api/lambda_function/get_data_functions/news_data_select.py
+++ src/database-api/lambda_function/get_data_functions/news_data_select.py
@@ -3,9 +3,9 @@ import get_data_functions.psycopg_methods as psycopg_methods
 def get_time_source_title_url_news_uid()->dict:
 
     sql = """
-        SELECT time, title, url, uid FROM news_data_select
-        LIMIT 50
-        ORDER BY time DESC;y
+        SELECT n.time, n.title, n.url, n.uid FROM news_data_select AS n
+        INNER JOIN opinion_extract_200_news AS o ON n.uid = o.news_uid
+        ORDER BY n.time DESC;
         """
     rows = psycopg_methods.execute_sql(sql)
 
--- src/react-amplified/src/components/AllPerson.jsx
+++ src/react-amplified/src/components/AllPerson.jsx
@@ -2,48 +2,56 @@ import { useState, useEffect } from 'react';
 
 import { Link } from 'react-router-dom';
 
+// import { Divider } from "antd";
+
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 
-// import { gMapToObject, } from '../utility/GraphSON';
-
-
-
-
 
 
 function TitlebarBelowImageList(props) {
-    const [data, setData] = useState(props.category_data);
-    const category = props.category;
+    const [categorydata, setCategoryData] = useState(props.category_data);
+    const [category, setCategory] = useState(props.category);
+
+    useEffect(() => {
+        setCategoryData(props.category_data);
+        setCategory(props.category);
+    }, [props.category_data, props.category]);
     
-    if (!data) return "No data!";
+    if (!categorydata) return "No data!";
 
     return (
-        <ImageList sx={{ width: 1 / 1, height: 900 }} cols={7}>
-            {data.map((item) => (
-                <ImageListItem key={item['name']} component={Link} to={'/person/' + item['name']}>
-                    <img
-                        src={item['daily_image_url'] + "?w=248&fit=crop&auto=format"}
-                        srcSet={item['daily_image_url'] + "?w=248&fit=crop&auto=format&dpr=2 2x"}
-                        alt={item['name']}
-                        loading="lazy"
-                    />
-                    <ImageListItemBar
-                        title={item['name']}
-                        subtitle={
-                            <div>
-                                <p>{item['daily_category']}</p>
-                                <p>{item['title'] && "任職於"+item['title']}</p>
-                                <p>{item['party'] && item['party']}</p>
-                            </div>
-                        }
-                        position="bottom"
-                        key={item['name']}
-                    />
-                </ImageListItem>
-            ))}
-        </ImageList>
+        <>
+            <h2>{category} ({categorydata.length} 人)</h2>
+            <ImageList sx={{ width: 1 / 1, height: 900 }} cols={7}>
+                {categorydata.map((item) => (
+                    <>
+                        {/* <p>{item['daily_image_url']}</p> */}
+                        <ImageListItem key={item['name']} component={Link} to={'/person/' + item['name']}>
+                            <img
+                                src={item['dailyview_image_url'] + "?w=248&fit=crop&auto=format"}
+                                srcSet={item['dailyview_image_url'] + "?w=248&fit=crop&auto=format&dpr=2 2x"}
+                                alt={item['name']}
+                                loading="lazy"
+                            />
+                            <ImageListItemBar
+                                title={item['name']}
+                                subtitle={
+                                    <div>
+                                        <p>{item['dailyview_category']}</p>
+                                        <p>{item['title'] && "任職於"+item['title']}</p>
+                                        <p>{item['party'] && item['party']}</p>
+                                    </div>
+                                }
+                                position="bottom"
+                                key={item['name']}
+                            />
+                        </ImageListItem>
+                    </>
+                ))}
+            </ImageList>
+        </>
     );
 }
 
@@ -51,54 +59,44 @@ export default function AllPerson(props) {
 
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    const [data, setData] = useState(null);
+    const [person_data, setPersonData] = useState({});
 
     const category_list = ['政治人物', '地方首長', '立法委員', '陳揆內閣', '台北市議員', '高雄市議員', '政論名嘴'];
-    const post_url = "https://6pd8cpbd2g.execute-api.us-east-1.amazonaws.com/Prod/person?="
+    const post_url = "https://6pd8cpbd2g.execute-api.us-east-1.amazonaws.com/Prod/person?category="
 
 
     useEffect(() => {
-        let person_data = {}; 
         for (let i = 0; i < category_list.length; i++) {
-            console.log(category_list[i]);
-            fetch(post_url, { method: 'POST', body: JSON.stringify({'category': category_list[i]})})
+            fetch(post_url + category_list[i], { method: 'POST'})
                 .then((response) => response.json())
                 .then((data) => {
-                    console.log(data);
-                    // person_data[category_list[i]] = data;
-                    // person_data[category_list[i]] = data['data'];
+                    console.log()
+                    setPersonData((person_data) => ({...person_data, [category_list[i]]: data}));
                     setLoading(false);
+                    console.log(category_list[i])
                 })
-                .catch((error) => {
-                    setError(error);
-                    setLoading(false)
-                });
+                // .catch((error) => {
+                //     setError(error);
+                //     setLoading(false)
+                // });
         }
-        setData(person_data);
     }, []);
-    console.log("person_data");
-    console.log(person_data);
+
+
     if (loading) return "Loading...";
-    if (error) return "Error!";
-    if (data) return "No data!";
+    // if (error) return "Error!";
+    // if (person_data) return "No data!";
     
 
     return (
         <div className="all-person">
-            <h1>所有人物</h1>
-            <h2>政治人物 ({person_data['政治人物'].length} 人)</h2>
+            {/* <Divider orientation="left">所有人物</Divider> */}
             <TitlebarBelowImageList category='政治人物' category_data={person_data['政治人物']}></TitlebarBelowImageList>
-            <h2>地方首長 ({person_data['地方首長'].length} 人)</h2>
             <TitlebarBelowImageList category='地方首長' category_data={person_data['地方首長']}></TitlebarBelowImageList>
-            <h2>立法委員 ({person_data['立法委員'].length} 人)</h2>
             <TitlebarBelowImageList category='立法委員' category_data={person_data['立法委員']}></TitlebarBelowImageList>
-            <h2>蘇揆內閣 ({person_data['陳揆內閣'].length} 人)</h2>
             <TitlebarBelowImageList category='陳揆內閣' category_data={person_data['陳揆內閣']}></TitlebarBelowImageList>
-            <h2>台北市議員 ({person_data['台北市議員'].length} 人)</h2>
             <TitlebarBelowImageList category='台北市議員' category_data={person_data['台北市議員']}></TitlebarBelowImageList>
-            <h2>高雄市議員 ({person_data['高雄市議員'].length} 人)</h2>
             <TitlebarBelowImageList category='高雄市議員' category_data={person_data['高雄市議員']}></TitlebarBelowImageList>
-            <h2>政論名嘴 ({person_data['政論名嘴'].length} 人)</h2>
             <TitlebarBelowImageList category='政論名嘴' category_data={person_data['政論名嘴']}></TitlebarBelowImageList>
         </div>
     );
