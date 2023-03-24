0c946c0e31b52721d9fcc072d136f9377d7fcf7b
tc-huang <jameshuang890902@gmail.com>
2023-03-25 00:35:48 +0800

    chore: Update

---
+++ src/spacy_pipeline/pipeline_setup.py
@@ -0,0 +1,40 @@
+from spacy.tokens import Token, Span, Doc
+import spacy_stanza
+import torch
+from .ckip import ckip_ner, ckip_pos
+
+has_gpu = True if torch.cuda.is_available() else False
+
+def set_all_extensions():
+    Token.set_extension('label_id', default=[], force=True)
+    Token.set_extension('label_type', default=[], force=True)
+    Token.set_extension('relation_label', default=[], force=True)
+
+    Span.set_extension('label_id', default=[], force=True)
+    Span.set_extension('label_type', default=[], force=True)
+    Span.set_extension('relation_label', default=[], force=True)
+
+    Doc.set_extension('news_uid', default=None, force=True)
+    Doc.set_extension('news_title', default=None, force=True)
+    Doc.set_extension('news_url', default=None, force=True)
+    Doc.set_extension('paragraph_index', default=None, force=True)
+
+
+def error_handler(proc_name, proc, docs, e):
+    print(f"An error occurred when applying component {proc_name}.")
+    print(f"Docs: {docs}")
+    print(f"Proc: {proc}")
+    print(f"Error: {e}")
+    print()
+
+
+def get_pipeline():
+    set_all_extensions()
+    spacy_pipeline = spacy_stanza.load_pipeline("xx", lang='zh-hant', use_gpu=has_gpu)
+    spacy_pipeline.add_pipe('ckip_pos', last=True)
+    spacy_pipeline.add_pipe('ckip_ner', last=True)
+    spacy_pipeline.set_error_handler(error_handler)
+    print(spacy_pipeline.pipe_names)
+    analysis = spacy_pipeline.analyze_pipes(pretty=True)
+    print(analysis)
+    return spacy_pipeline
